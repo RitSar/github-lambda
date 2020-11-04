1b6fe3f2e37600a73d63bee194bd7b93e6735e7e
RitSar <55022926+RitSar@users.noreply.github.com>
2020-11-04 15:52:51 +0530

    added repositories

--- functions/getuser.js
+++ functions/getuser.js
@@ -1,10 +1,23 @@
 'use-strict'
 const axios = require('axios');
 require('dotenv').config()
-let URL;
+
+let userDataUrl,
+  userRepoUrl;
+
+function sortByProperty(property) {
+  return function(a, b) {
+    if (a[property] < b[property]) 
+      return 1;
+    else if (a[property] > b[property]) 
+      return -1;
+    
+    return 0;
+  }
+}
 
 exports.handler = function(event, context, callback) {
-  
+
   const send = body => {
     callback(null, {
       statusCode: 200,
@@ -14,20 +27,40 @@ exports.handler = function(event, context, callback) {
       },
       body: JSON.stringify(body)
     });
-}
-  
+  }
+
   if (event.httpMethod == 'POST') {
     const parsedBody = JSON.parse(event.body);
-    console.log(parsedBody);
     const USER = parsedBody.username;
     const API_URL = `https://api.github.com/users/${USER}`;
-    console.log(API_URL);
-    URL = `${API_URL}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
-    console.log(URL);
+    userDataUrl = `${API_URL}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
+    userRepoUrl = `${API_URL}/repos?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
+
   } else if (event.httpMethod == 'GET') {
-    console.log(URL);
-    axios.get(URL)
-      .then(res => send(res.data))
-      .catch(err => send(err));
+    axios.all([
+     axios.get(userDataUrl),
+     axios.get(userRepoUrl)
+   ])
+     .then(axios.spread((userData, userRepos) => {
+      let data = userData.data;
+      let repoData = ((userRepos.data).map(repo => {
+       return {
+         name: repo.name,
+         description: repo.description,
+         url: repo.svn_url,
+         size: repo.size,
+         stars: repo.stargazers_count,
+         language: repo.language,
+         forks: repo.forks
+       }
+     })) 
+     repoData.sort(sortByProperty("stars"));
+     data.repos = repoData;
+     send(data);
+     
+   })).catch(err => {
+     console.log(err);
+     send(err);
+   });
   }
 }
--- js/user.js
+++ js/user.js
@@ -1,13 +1,43 @@
-const fetchUser = async () => await(await fetch("http://localhost:9000/getuser")).json();
+const fetchUser = async () => await(await fetch("/.netlify/functions/getuser")).json();
 fetchUser().then(data => {
-  console.log(data);
-  $("#profile_image").attr("src",data.avatar_url);
-  $("#username").text(data.name);
+  if (data.name == "Error") {
+    if (window.confirm('User was not found. Click OK to go back to input page.')) {
+      window.location.href = '/';
+    };
+  }
+  $("#profile_image").attr("src", data.avatar_url);
+  if (data.name) {
+    let $temp = $('<h3 id="username">Username</h3>');
+    $(".container").append($temp);
+    $("#username").text(data.name);
+  }
   $("#name").text('@' + data.login);
-  $("#name").attr('href',data.html_url);
-  $("#location").text(data.location);
-  $("#created").text(data.created_at);
+  $("#name").attr('href', data.html_url);
+  if (data.location) {
+    let $temp = $('<div class="col s12 m4"><i class="fa fa-map-marker" aria-hidden="true"></i> <span id="location"></span></div>');
+    $(".row1").append($temp);
+    $("#location").text(data.location);
+  }
+  if (data.company) {
+    let $temp = $('<div class="col s12 m4"><i class="fa fa-briefcase" aria-hidden="true"></i> <span id="company"></span></div>');
+    $(".row1").append($temp);
+    $("#company").text(data.company);
+  }
+  if (data.created_at) {
+    const event = new Date(data.created_at);
+    let $temp = $(`<div class="col s12 m4"><i class="fa fa-calendar" aria-hidden="true"></i> <span id="created">Joined on ${event.toString().slice(0, 15)}</span></div>`);
+    $(".row1").append($temp);
+  }
+
   $("#public_repos").text(data.public_repos);
   $("#followers").text(data.followers);
   $("#following").text(data.following);
+
+  jQuery.each(data.repos, function(i, val) {
+    let $temp = $(`<div class="col s12 m4 l2"><div class="card repo"><div class="card-content"><a href=${val.url} target="_blank"><span class="card-title">${val.name}</span></a><p>${val.description}</p></div><div class="card-action"><span>${val.language}</span><span><i class="fa fa-star" aria-hidden="true"></i> ${val.stars}</span><span><i class="fa fa-code-fork" aria-hidden="true"></i> ${val.forks}</span><span id="size">${val.size} kb</span></div></div></div>`);
+    $(".row3").append($temp);
+    if (i == 5 || i == val.length) 
+      return false;
+    }
+  );
 })
